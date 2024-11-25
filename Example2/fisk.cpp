a9fa39d0e104a487c47bd885024c1c0afa81b742
Mikael <Mikael.bjorkman@aspire.se>
2024-11-25 08:41:44 +0100

    Add examples

---
+++ Example2/fisk.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+void print_fisk()
+{
+	printf("Fisk!\n");
+}
---
+++ Example3/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "fisk.hpp"
+
+int main()
+{
+	std::cout << "Hello, class\n";
+	print_fisk();
+	return 0;
+}
